79ce37e42457ca18e06e2d0233b1809de7e0c5df
Ludovico Rossi <ludo@icecreamstudios.com>
2015-11-11 16:55:34 +0100

    Add loading HUD while running an example

--- libxlsxwriter-ObjC/libxlsxwriter-ObjC/ExampleViewerViewController.m
+++ libxlsxwriter-ObjC/libxlsxwriter-ObjC/ExampleViewerViewController.m
@@ -9,9 +9,14 @@
 #import "ExampleViewerViewController.h"
 #import "Example.h"
 
-@interface ExampleViewerViewController ()
+// The minimum time the loading spinning wheel is shown for (in order to avoid a quick flashing when the loading time is too short)
+static NSTimeInterval const kMinLoadingTime = 0.3;
+
+
+@interface ExampleViewerViewController () <UIWebViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicatorView;
 
 @end
 
@@ -23,15 +28,31 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
+    // set title
     self.navigationItem.title = self.example.title;
+    
+    // start activity indicator
+    self.view.userInteractionEnabled = NO;
+    [self.activityIndicatorView startAnimating];
+    
+    // store current time interval before running the example
+    NSTimeInterval beforeRun = [NSDate timeIntervalSinceReferenceDate];
     
     __weak typeof(self) weakSelf = self;
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        // run example and generate output Excel file
         [weakSelf.example run];
         
-        dispatch_async(dispatch_get_main_queue(), ^{
+        // calculate time elapsed while running the example
+        NSTimeInterval elapsedTime = [NSDate timeIntervalSinceReferenceDate] - beforeRun;
+        
+        // calculate time we still need to wait (if any) before displaying the output Excel file
+        NSTimeInterval timeToWaitFor = MAX(kMinLoadingTime - elapsedTime, 0.0);
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(timeToWaitFor * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             if (weakSelf) {
+                // display output Excel file in the web view
                 NSURL *fileURL = [NSURL fileURLWithPath:weakSelf.example.outputFilePath];
                 NSURLRequest *request = [NSURLRequest requestWithURL:fileURL];
                 
@@ -41,9 +62,18 @@
     });
 }
 
+#pragma mark - Web View Delegate
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    // stop activity indicator
+    [self.activityIndicatorView stopAnimating];
+    self.view.userInteractionEnabled = YES;
+}
+
 #pragma mark - Action Button
 
 - (IBAction)didTapActionButton:(UIBarButtonItem *)sender {
+    // show default options for the output Excel file
     NSURL *fileURL = [NSURL fileURLWithPath:self.example.outputFilePath];
     UIDocumentInteractionController *controller = [UIDocumentInteractionController interactionControllerWithURL:fileURL];
     [controller presentOptionsMenuFromBarButtonItem:sender animated:YES];
