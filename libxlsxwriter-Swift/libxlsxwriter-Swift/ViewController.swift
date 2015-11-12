caae0ffb1bdfeeb19741e9f0fabe80d4006cae18
Ludovico Rossi <ludo@icecreamstudios.com>
2015-11-12 13:24:23 +0100

    Add skeleton for swift project

---
+++ libxlsxwriter-Swift/libxlsxwriter-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  libxlsxwriter-Swift
+//
+//  Created by Ludovico Rossi on 12/11/15.
+//  Copyright © 2015 Ludovico Rossi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
